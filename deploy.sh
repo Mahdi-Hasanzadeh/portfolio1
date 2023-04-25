d850e013889134613564218c3a17ea0f093d00e7
Mahdi Hasanzadeh <Mahdi.h2619@gmail.com>
2023-04-25 18:18:39 +0430

    commit

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+#if you are deploying to a custom domain
+#echo 'www.example.com' > CNAME
+
+git init
+git checkout -B amin
+git add -A
+git commit -m 'deploy'
+
+#if you are deploying to https://<USERNAME>.github.io
+#git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+#if you are deploying to https://<USERNAME>.github.io/<REPO>
+#git push -f git@github.com:Mahdi-Hasanzadeh/portfolio.git main:gh-pages
+
+cd-
